e61666e23bc96c9df4fba68d9ea32112cae31efa
xtesl <146335490+xtesl@users.noreply.github.com>
2024-04-11 19:39:50 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,40 +1,45 @@
-const url = 'https://learnify-api-kps0.onrender.com/api/v1/auth/confirm-user/'; // Replace with your API endpoint
+const url = 'https://learnify-api-kps0.onrender.com/api/v1/auth/login/'; // Replace with your API endpoint
 
-email = document.getElementById('email');
-code = document.getElementById('password');
-button = document.getElementById('submit');
+const submit = document.getElementById('submit');
 
-const data = {
-  'email': email.value,
-  'otp': code.value
-};
+submit.addEventListener('click', function(event) {
+  event.preventDefault(); // Prevent the default form submission
 
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
 
-const options = {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json'
-    // Add any other headers you need, such as authorization headers
-  },
-  body: JSON.stringify(data)
-};
+  const data = {
+    email: email,
+    password: password
+  };
 
-function activate(){
+  const options = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+      // Add any other headers you need, such as authorization headers
+    },
+    body: JSON.stringify(data)
+  };
+ loginMessage.style.display = 'block';
+  submit.disabled = true;
   fetch(url, options)
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-    return response.json();
-  })
-  .then(data => {
-    windows.alert('User activation was successful');
-    // Handle the response data here
-  })
-  .catch(error => {
-    console.error('There was a problem connecting to the server please try again later', error);
-    // Handle errors here
-  });
-};
-
-button.addEventListener('click', activate);
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then(data => {
+      window.alert('Confirmation successful');
+      // Handle the response data here
+    })
+    .catch(error => {
+      console.error('There was a problem with the POST request:', error);
+      // Handle errors here
+    }) .finally(() => {
+      // Hide the login message and enable the submit button
+      loginMessage.style.display = 'none';
+      submit.disabled = false;
+    });
+});
